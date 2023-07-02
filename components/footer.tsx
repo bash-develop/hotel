9097740edd27315f0ff0e8d57db3cad964613cd2
Bogdan <shankin.dev@gmail.com>
2023-07-02 20:13:19 +0300

    йцук

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,10 +8,11 @@ function Footer() {
 				<span className="block"> Отель София</span>
 				<span className="block">2023, Официальный сайт</span>
 			</div>
-			<div className="location flex gap-3 items-center hover:text-orange-400">
+			<div className="location hidden md:flex gap-3 items-center hover:text-orange-400">
 				<FaLocationDot />
-				<a href="https://yandex.ru/maps/11063/mineralniye-vodi/?from=mapframe&ll=43.118770%2C44.219193&mode=usermaps&source=mapframe&um=constructor%3A3ad3a4e6b45fe957354068aff46726eae093200b393622ad7047f366790abfd9&utm_source=mapframe&z=15">улица Железноводская, д.14, Минеральные Воды</a>
-				
+				<a href="https://yandex.ru/maps/11063/mineralniye-vodi/?from=mapframe&ll=43.118770%2C44.219193&mode=usermaps&source=mapframe&um=constructor%3A3ad3a4e6b45fe957354068aff46726eae093200b393622ad7047f366790abfd9&utm_source=mapframe&z=15">
+					улица Железноводская, д.14, Минеральные Воды
+				</a>
 			</div>
 		</div>
 	);
