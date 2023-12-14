e1bebb345f00773cd9ae409f980ac86fa53f6c1a
Bogdan <98741979+sbacorp@users.noreply.github.com>
2023-12-14 16:10:28 +0300

    12312123123

--- app/page.tsx
+++ app/page.tsx
@@ -64,6 +64,7 @@ export default function Home() {
       root
       ref={lenisRef}
       options={{
+        smoothTouch: true,
         SmoothScroll: true,
         duration: 1.5,
         easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
